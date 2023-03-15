a39071bc355c24c6b94c6853090d86499a855359
wostrau <a.ostrovtsov@gmail.com>
2023-03-16 00:17:13 +0100

    Websocket chat - #1 video-lesson / stopped at 01:50:00

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,43 @@
-import React, {useEffect, useState} from 'react'
+import React, {useEffect, useRef, useState} from 'react'
 import './App.css'
 
 type userType = {
     userId: number
     userName: string
-    photo: string
     message: string
+    photo: string
 }
 
 export const App: React.FC = () => {
+    const messagesBlockRef = useRef()
     const [message, setMessage] = useState<string>('')
     const [webSocket, setWebSocket] = useState<null | WebSocket>(null)
+    if (webSocket) {
+        webSocket.onmessage = (messageEvent) => {
+            const downloadedUsers = JSON.parse(messageEvent.data)
+            setUsers([...users, ...downloadedUsers])
+            if (messagesBlockRef.current) messagesBlockRef.current.scrollTo(0, messagesBlockRef.current.scrollHeight)
+        }
+    }
     const [users, setUsers] = useState<userType[]>([])
 
     useEffect(() => {
         const localWebSocket = new WebSocket('wss://social-network.samuraijs.com/handlers/ChatHandler.ashx')
-        localWebSocket.onmessage = (messageEvent) => {
-            const messages = JSON.parse(messageEvent.data)
-            setUsers(messages)
-        }
         setWebSocket(localWebSocket)
     }, [])
 
     const sendMessage = () => {
         webSocket?.send(message)
+        setMessage('')
     }
 
     return (
         <div className="App">
             <div className="chat">
-                <div className="messages">
-                    {users.map(u => {
+                <div className="messages" ref={messagesBlockRef}>
+                    {users.map((u, index) => {
                         return (
-                            <div key={u.userId} className="message">
+                            <div key={index} className="message">
                                 <img
                                     src={u.photo}
                                     alt="avatar"/>
@@ -48,7 +53,7 @@ export const App: React.FC = () => {
                             cols={30}
                             rows={5}
                             onChange={(e) => setMessage(e.currentTarget.value)}
-                            defaultValue={message}
+                            value={message}
                         />
                     <button
                         onClick={sendMessage}
