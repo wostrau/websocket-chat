5b713ae9316d23b48bba2ff26339f933b6737ca2
wostrau <a.ostrovtsov@gmail.com>
2023-03-15 18:19:19 +0100

    Websocket chat - #1 video-lesson / stopped at 00:53:00

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+type userType = {
+    id: number
+    name: string
+    photo: string
+    message: string
 }
 
-export default App;
+export const App: React.FC = () => {
+    const [message, setMessage] = useState<string>('')
+    const [users, setUsers] = useState<userType[]>([
+        {id: 1, name: 'Sanya', photo: 'https://via.placeholder.com/100', message: 'I\'m here!'},
+        {id: 2, name: 'Masha', photo: 'https://via.placeholder.com/100', message: 'I\'m not OK with this('}
+    ])
+
+    useEffect(() => {
+        const webSocket = new WebSocket('wss://social-network.samuraijs.com/handlers/ChatHandler.ashx')
+        webSocket.onmessage = () => {
+        }
+    }, [])
+
+    const sendMessage = () => {
+        alert(message)
+    }
+
+    return (
+        <div className="App">
+            <div className="chat">
+                <div className="messages">
+                    {users.map(u => {
+                        return (
+                            <div key={u.id} className="message">
+                                <img
+                                    src={u.photo}
+                                    alt="avatar"/>
+                                <b>{u.name}</b>
+                                <span>{u.message}</span>
+                            </div>
+                        )
+                    })}
+                </div>
+                <div className="footer">
+                        <textarea
+                            name="newMessage"
+                            cols={30}
+                            rows={5}
+                            onChange={(e) => setMessage(e.currentTarget.value)}
+                            defaultValue={message}
+                        />
+                    <button
+                        onClick={sendMessage}
+                    >SEND
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+}
