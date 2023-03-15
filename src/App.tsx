cd42be687da43dc5117bad4cd7b19d4eb6f128e2
wostrau <a.ostrovtsov@gmail.com>
2023-03-15 22:46:39 +0100

    Websocket chat - #1 video-lesson / stopped at 01:20:00

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,28 @@ import React, {useEffect, useState} from 'react'
 import './App.css'
 
 type userType = {
-    id: number
-    name: string
+    userId: number
+    userName: string
     photo: string
     message: string
 }
 
 export const App: React.FC = () => {
     const [message, setMessage] = useState<string>('')
-    const [users, setUsers] = useState<userType[]>([
-        {id: 1, name: 'Sanya', photo: 'https://via.placeholder.com/100', message: 'I\'m here!'},
-        {id: 2, name: 'Masha', photo: 'https://via.placeholder.com/100', message: 'I\'m not OK with this('}
-    ])
+    const [webSocket, setWebSocket] = useState<null | WebSocket>(null)
+    const [users, setUsers] = useState<userType[]>([])
 
     useEffect(() => {
-        const webSocket = new WebSocket('wss://social-network.samuraijs.com/handlers/ChatHandler.ashx')
-        webSocket.onmessage = () => {
+        const localWebSocket = new WebSocket('wss://social-network.samuraijs.com/handlers/ChatHandler.ashx')
+        localWebSocket.onmessage = (messageEvent) => {
+            const messages = JSON.parse(messageEvent.data)
+            setUsers(messages)
         }
+        setWebSocket(localWebSocket)
     }, [])
 
     const sendMessage = () => {
-        alert(message)
+        webSocket?.send(message)
     }
 
     return (
@@ -31,11 +32,11 @@ export const App: React.FC = () => {
                 <div className="messages">
                     {users.map(u => {
                         return (
-                            <div key={u.id} className="message">
+                            <div key={u.userId} className="message">
                                 <img
                                     src={u.photo}
                                     alt="avatar"/>
-                                <b>{u.name}</b>
+                                <b>{u.userName}</b>
                                 <span>{u.message}</span>
                             </div>
                         )
